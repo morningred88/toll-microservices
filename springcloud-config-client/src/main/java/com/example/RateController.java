0a4d2a1322d24f28e55c0be0201a8edef3bb5b80
Morningred <xma50089@gmail.com>
2021-02-12 20:22:11 -0600

    Added config server, client and configuration files

---
+++ springcloud-config-client/src/main/java/com/example/RateController.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RefreshScope
+@Controller
+public class RateController {
+	
+	
+	@Value("${rate}")
+	String rate;
+	@Value("${lanecount}")
+	String lanecount;
+	@Value("${tollstart}")
+	String tollstart;
+	
+	@RequestMapping("/rate")
+	public String getRate(Model model) {
+		model.addAttribute("rateamount", rate);
+		model.addAttribute("lanes", lanecount);
+		model.addAttribute("tollstart", tollstart);
+		
+		return "rateview";
+	}
+
+}
